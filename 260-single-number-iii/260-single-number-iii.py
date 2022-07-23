d09cb987c81191a1793a97be7eac6d5ec2835583
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-23 17:40:02 -0400

    Time: 80 ms (74.09%), Space: 15.7 MB (54.63%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.py
@@ -0,0 +1,23 @@
+#5 : 30
+
+class Solution:
+    def singleNumber(self, a: List[int]) -> List[int]:
+        res = [0, 0]
+        xor = 0
+        for i in a:
+            xor ^= i
+            
+        index = -1
+        for j in range(32):
+            if (xor & (1 << j)) > 0:
+                index = j
+                
+        for i in a:
+            if (i & (1 << index)) > 0:
+                res[0] ^= i
+            
+        res[1] = res[0]
+        for i in a:
+            res[1] ^= i
+        
+        return res
