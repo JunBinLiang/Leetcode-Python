6de5a4a36f6cfcec3a33b8280a03f723c9400ac7
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-23 22:22:52 -0400

    Time: 42 ms (69.04%), Space: 13.8 MB (73.29%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.py
@@ -0,0 +1,22 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        self.dp = []
+        for i in range(m):
+            a = []
+            for j in range(n):
+                a.append(-1)
+            self.dp.append(a)
+        
+        return self.recursion(m - 1, n - 1)
+        
+    def recursion(self, m,n):
+        if m == 0 or n == 0:
+            return 1
+        if self.dp[m][n] != -1:
+            return self.dp[m][n]
+        
+        
+        self.dp[m][n] = self.recursion(m-1,n)+self.recursion(m, n-1)
+        return self.dp[m][n]
+
+        
