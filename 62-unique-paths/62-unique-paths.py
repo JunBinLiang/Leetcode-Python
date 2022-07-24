10a0f486a8fe1a725c2d5eaabfe457ca88bb5a9b
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-23 22:25:54 -0400

    Time: 27 ms (98.23%), Space: 13.8 MB (73.29%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -9,13 +9,11 @@ class Solution:
         
         return self.recursion(m - 1, n - 1)
         
-    def recursion(self, m,n):
+    def recursion(self, m,n): #O(mn)
         if m == 0 or n == 0:
             return 1
         if self.dp[m][n] != -1:
             return self.dp[m][n]
-        
-        
         self.dp[m][n] = self.recursion(m-1,n)+self.recursion(m, n-1)
         return self.dp[m][n]
 
