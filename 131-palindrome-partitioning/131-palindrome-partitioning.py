fb1403a60d97311e44557732bbfc98ee99027e9a
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-25 00:09:16 -0400

    Time: 820 ms (70.53%), Space: 30 MB (71.27%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,28 +1,18 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        self.res=[]
-        self.list1=list(s)
-        self.dfs(0,"",[],s)
+        self.res = []
+        self.dfs(s, 0, [])
         return self.res
-    
-    def dfs(self,i,tmpString,tmpList,s):
-        if i==len(s):
-            for p in tmpList:
-                if p!=p[::-1]:
-                    return
-            le = 0
-            for j in tmpList:
-                le += len(j)
-            if le == len(s):
-                self.res.append(tmpList.copy())
+        
+    def dfs(self, s, idx, li):
+        if idx == len(s):
+            self.res.append([x for x in li])
             return
         
-       
-    
-    
-        tmpString=tmpString+s[i]
-        self.dfs(i+1,tmpString,tmpList,s)
-        
-        tmpList.append(tmpString)
-        self.dfs(i+1,"",tmpList,s)
-        tmpList.pop()
+        for j in range(idx, len(s)):
+            #s[idx : j]
+            newstr = s[idx : j + 1]
+            if newstr == newstr[::-1]:
+                li.append(newstr)
+                self.dfs(s, j + 1, li)
+                li.pop()
