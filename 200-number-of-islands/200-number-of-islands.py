2a6c25d42e948649274ee5dcb0b1e6cf7e492707
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-24 22:17:45 -0400

    Time: 630 ms (18.40%), Space: 16.4 MB (80.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,6 +1,3 @@
-import collections
-di = [[1, 0],[-1, 0],[0, 1],[0, -1]]
-
 class Solution:
     def numIslands(self, a: List[List[str]]) -> int:
         n = len(a)
@@ -8,23 +5,21 @@ class Solution:
         cnt = 0
         for i in range(n):
             for j in range(m):
-                if a[i][j] == '0':
-                    continue
-                
-                # do BFS
-                cnt += 1
-                q = deque([[i, j]])
-                a[i][j] = '0'
-                while len(q) > 0:
-                    sz = len(q)
-                    for x in range(sz):
-                        pos = q.popleft()
-                        r, c = pos[0], pos[1]
-                        for p in di:
-                            nxtr = r + p[0]
-                            nxtc = c + p[1]
-                            if nxtr < 0 or nxtr >= n or nxtc < 0 or nxtc >= m or a[nxtr][nxtc] == '0':
-                                continue
-                            q.append([nxtr, nxtc])
-                            a[nxtr][nxtc] = '0'
+                if a[i][j] == '1':
+                    cnt += 1
+                    self.dfs(a, i, j)
         return cnt
+    
+    def dfs(self, a, r, c):
+        n = len(a)
+        m = len(a[0])
+        if r < 0 or r >= n or c < 0 or c >= m:
+            return
+        if a[r][c] == '0':
+            return
+        
+        a[r][c] = '0'
+        self.dfs(a, r + 1, c)
+        self.dfs(a, r - 1, c)
+        self.dfs(a, r, c + 1)
+        self.dfs(a, r, c - 1)
