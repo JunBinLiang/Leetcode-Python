2d25d00cd91867a1fa0ebdc1ca16f5db752d1966
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-25 23:05:25 -0400

    Time: 1875 ms (50.86%), Space: 18.6 MB (30.29%) - LeetHub

---
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        res = 0
+        odd, even = 0, 1
+        sum1 = 0
+        for i in arr:
+            sum1 += i
+            if sum1 % 2 == 1:
+                res += even
+                odd += 1
+            else:
+                res += odd
+                even += 1
+        
+        
+        return res % 1000000007
