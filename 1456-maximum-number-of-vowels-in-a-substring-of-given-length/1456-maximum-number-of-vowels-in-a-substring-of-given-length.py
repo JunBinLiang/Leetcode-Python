552134def72ba81bb2cda4ce5e6fcb317cf7468c
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-08-13 01:08:21 -0400

    Time: 346 ms (36.75%), Space: 14.8 MB (39.96%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -7,14 +7,16 @@ class Solution:
             return False
         
         res, cnt = 0, 0
+        j = 0
         for i in range(len(s)):
             if good(s[i]):
                 cnt += 1
-            if i < k - 1:
-                continue  
-            res = max(res, cnt)    
-            if good(s[i - k + 1]):
-                cnt -= 1
+            while i - j + 1 >= k:
+                res = max(res, cnt)
+                if good(s[j]):
+                    cnt -= 1
+                j += 1
+                
         return res
             
                 
