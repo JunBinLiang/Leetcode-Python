6a12e1fa4a874c55529db862572b3ed0ae41a013
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-08-24 00:01:50 -0400

    Time: 5090 ms (59.70%), Space: 32.6 MB (33.35%) - LeetHub

--- 827-making-a-large-island/827-making-a-large-island.py
+++ 827-making-a-large-island/827-making-a-large-island.py
@@ -1,54 +1,59 @@
 di = [[0, 1],[0, -1],[1, 0],[-1, 0]]
 class Solution:
     def largestIsland(self, a: List[List[int]]) -> int:
-        n = len(a)
-        m = len(a[0])
-        ID = 1
-        self.cnt = 0
+        def allone(ma):
+            for row in ma:
+                if sum(row) != len(ma[0]):
+                    return False
+            return True
+        
+        n, m = len(a), len(a[0])
+        if allone(a):
+            return n * m
         res = 0
+        def find(nums, x):
+            if nums[x] == x:
+                return x
+            root = find(nums, nums[x])
+            nums[x] = root
+            return root
         
-        mark = []
-        for i in range(n):
-            mark.append([0] * m)
-        
-        
-        def dfs(r, c):
-            if r < 0 or r >= n or c < 0 or c >= m:
-                return
-            if a[r][c] == 0 or mark[r][c] != 0:
-                return
-            mark[r][c] = ID
-            self.cnt += 1
-            for d in di:
-                nxtr = r + d[0]
-                nxtc = c + d[1]
-                dfs(nxtr, nxtc)
-                
-          
-        f = {}
-        for i in range(n):
-            for j in range(m):
-                if a[i][j] == 1 and mark[i][j] == 0:
-                    self.cnt = 0
-                    dfs(i, j)
-                    f[ID] = self.cnt
-                    ID += 1
-                    res = max(res, self.cnt)
-                    
+        nums = [0] * (n * m)
+        sizes = [1] * (n * m)
+        for i in range(n * m):
+            nums[i] = i
+            
+           
         for i in range(n):
             for j in range(m):
                 if a[i][j] == 0:
-                    tot = 1
+                    continue
+                r1 = find(nums, i * m + j)
+                for d in di:
+                    nxtr = i + d[0]
+                    nxtc = j + d[1]
+                    if nxtr >= 0 and nxtr < n and nxtc >= 0 and nxtc < m and a[nxtr][nxtc] == 1:
+                        r2 = find(nums, nxtr * m + nxtc)
+                        if r1 != r2:
+                            nums[r2] = r1
+                            sizes[r1] += sizes[r2]
+                            res = max(res, sizes[r1])
+                            
+        for i in range(n):
+            for j in range(m):
+                if a[i][j] == 0:
                     se = set()
+                    tot = 1
                     for d in di:
                         nxtr = i + d[0]
                         nxtc = j + d[1]
                         if nxtr >= 0 and nxtr < n and nxtc >= 0 and nxtc < m and a[nxtr][nxtc] == 1:
-                            se.add(mark[nxtr][nxtc])
+                            r1 = find(nums, nxtr * m + nxtc)
+                            se.add(r1)
                     for com in se:
-                        tot += f[com]
+                        tot += sizes[com]
                     res = max(res, tot)
         return res
-                
-#1 0
-#0 1
+        
+        
+        
